54ae2d57e05d25af009062c695215c0d203d8185
unknown <dhrumikansara1999@example.com>
2021-11-29 11:56:28 +0530

    backend setup

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const env = require("dotenv")
 const app = express()
+env.config()
 
-const PORT = 4000
+app.get("/api/test", (req, res)=>{
+  console.log(" test is successfull ")
+})
+
+mongoose.connect(process.env.MONGO_URL) 
+        .then(()=>console.log("Database connected successfully"))
+        .catch((e)=>console.log(e))
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`server started at port ${PORT}`)
 })
