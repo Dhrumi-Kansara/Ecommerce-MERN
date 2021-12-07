d7cc229f7968622674937efbff9abf988bd527c0
Dhrumi-Kansara <dhrumikansara1999@gmail.com>
2021-12-07 22:10:21 +0530

    frontend

--- ecommerce_react/src/components/Product.jsx
+++ ecommerce_react/src/components/Product.jsx
@@ -2,6 +2,20 @@ import styled from "styled-components";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+`;
 
 const Container = styled.div`
   flex: 1;
@@ -12,6 +26,11 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   background-color: #f5fbfd;
+  position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 const Circle = styled.div`
   width: 200px;
@@ -25,8 +44,24 @@ const Image = styled.img`
   height: 75%;
   z-index: 2;
 `;
-const Info = styled.div``;
-const Icon = styled.div``;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  cursor: pointer;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.1);
+  }
+`;
 
 const Product = ({ item }) => {
   return (
